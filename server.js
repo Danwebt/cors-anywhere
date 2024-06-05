07bc0f8d6aead4225c677afca80b995f598c92cc
Danwebt <dantotsuwebgh@gmail.com>
2024-06-05 18:43:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const host = '0.0.0.0';
 const port = process.env.PORT || 3000;
 
 cors_proxy.createServer({
-  originWhitelist: [], // Allow all origins
+  originWhitelist: ["https://https://dantest-seven.vercel.app"], // Allow all origins
   requireHeader: ['origin', 'x-requested-with'],
   removeHeaders: ['cookie', 'cookie2']
 }).listen(port, host, () => {
